ea0d4a920fec3a040672cd3f574eb20eb6702d5d
hori makoto <hori.makoto1209@gmail.com>
2025-02-21 04:31:21 +0900

    修正

--- main.js
+++ main.js
@@ -14,22 +14,14 @@ class LiverRandom{
 
     // csvを読み込んで配列化
     import_csv(){
-        let req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
-        req.open("get", "./data/liver.csv", true); // アクセスするファイルを指定
-        req.send(null); // HTTPリクエストの発行
-        
-        // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
-        req.onload = function(){
-            // convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
-            let str = req.responseText;
-            let result = []; // 最終的な二次元配列を入れるための配列
-            let tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
-        
-            // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
-            for(let i=0; i<tmp.length; ++i){
-                result[i] = tmp[i].split('  ');
-            }
-        }
+        let dataPath = "./data/liver.csv";
+        const request = new XMLHttpRequest(); // HTTPでファイルを読み込む
+        request.addEventListener('load', (event) => { // ロードさせ実行
+            const response = event.target.responseText; // 受け取ったテキストを返す
+            output_svg.innerHTML = response; // 表示
+        });
+        request.open('GET', dataPath, true); // csvのパスを指定
+        request.send();
         return result;
     }
     // ランダムでIDを決めて保存
